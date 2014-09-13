188aefac3cf176ce9d9c522b00936e1f44c113fc
stuart4 <stuart4@purdue.edu>
2014-09-12 20:13:57 -0400

    working implementation with lots of if's

--- LeapYear.java
+++ LeapYear.java
@@ -1,35 +1,32 @@
-import java.util.Scanner;
+/*
+ * LeapYear
+ * 
+ * computs if a year is a leap year or not
+ *
+ * @author Jacob Stuart
+ * 
+ * @lab 806
+ *
+ * 12 September, 2014
+ */
+
+//import java.util.*;
 public class LeapYear {
 
 	public static void main(String[] args) {
-		Scanner sin = new Scanner(System.in);
-
-		while (true){
-			if(!isLeapYear(sin.nextLong()))
-				System.out.println("Not a leap year");
-		}
+		//System.out.println(isLeapYear(200));
 	}
 
 	public static boolean isLeapYear (long year) {
-	
+		if (year % 400 == 0) {
+			return true;
+		}
 		if (year % 4 == 0) {
-
 			if (year % 100 == 0) {
-
-				if (year % 400 == 0) {
-
-					return true;
-
-				}
-				
 				return false;
-
 			}
 			return true;
-
-		}
+		}	
 		return false;
-
 	}
-
 }
