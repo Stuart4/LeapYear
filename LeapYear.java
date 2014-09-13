53c5df8a06200cbe4c2f88cddee6465df76159d6
stuart4 <stuart4@purdue.edu>
2014-09-12 20:37:14 -0400

    working, smoother implementation with a test

--- LeapYear.java
+++ LeapYear.java
@@ -10,13 +10,26 @@
  * 12 September, 2014
  */
 
-//import java.util.*;
+import java.util.*;
+import java.io.*;
 public class LeapYear {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) throws FileNotFoundException  {
+		Scanner sin = new Scanner(new File("./leaps.txt"));
+
+		while(sin.hasNextLong()) {
+			long year = sin.nextLong();
+
+			if (!isLeapYear(year)) {
+				System.out.printf("fail at %d\n", year);
+				System.exit(1);
+			}
+		}
+
 	}
 
 	public static boolean isLeapYear (long year) {
+		if (year % 400 == 0) return true;
 		if (year % 4 != 0 || year % 100 == 0)
 			return false;
 		return true;
